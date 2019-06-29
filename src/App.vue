47dff00a05cda546fa8eb6742bafc3dab186f55f
Megan Belzner <belzner@mit.edu>
2019-06-29 18:07:03 -0400

    Drag and resize thinky face features

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="logo" alt="Thinkifier" src="./assets/thinking-face.svg">
+    <Workspace></Workspace>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Workspace from './components/Workspace.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Workspace
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #d0dff7;
+}
+
+#logo {
+  margin: 30px 0;
+  position: relative;
+  width: 100px;
 }
 </style>
---
+++ src/components/Workspace.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="workspace" v-on:click="$root.$emit('remove-active')" v-on:dragover.prevent="doNothing" v-on:drop.prevent="doNothing">
+    <div class="background">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36">
+        <circle fill="#FFCB4C" cx="18" cy="17.018" r="17"/>
+      </svg>
+    </div>
+    <Feature type="left-eye"></Feature>
+    <Feature type="right-eye"></Feature>
+    <Feature type="mouth"></Feature>
+    <Feature type="hand"></Feature>
+  </div>
+</template>
+
+<script>
+import Feature from './Feature.vue'
+
+export default {
+  name: 'Workspace',
+  components: {
+    Feature
+  },
+  methods: {
+    doNothing: function () {}
+  }
+}
+</script>
+
+<style>
+.workspace {
+  margin: auto;
+  position: relative;
+  width: 500px;
+  height: 500px;
+  background-color: #ffffff;
+}
+.background {
+  width: 256px;
+  position: relative;
+  top: 122px;
+  left: 122px;
+}
+</style>
